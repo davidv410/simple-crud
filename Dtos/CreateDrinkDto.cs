08f60dd4d806a2fba0bcfc4ed85d0c5231b1e446
davidv410 <davidvrgoc99@gmail.com>
2026-07-18 16:19:53 +0200

    added validation

--- Dtos/CreateDrinkDto.cs
+++ Dtos/CreateDrinkDto.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SimpleCrud.Dtos;
 
 public record CreateDrinkDto(
-    string Name,
-    string Flavour,
-    decimal Price,
+    [Required][StringLength(50)]string Name,
+    [Required][StringLength(20)]string Flavour,
+    [Range(1, 100)]decimal Price,
     DateOnly ReleaseDate
 );
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using SimpleCrud.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation();
+
 var app = builder.Build();
 
 app.MapDrinksEndpoints();
