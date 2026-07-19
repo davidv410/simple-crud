9d04787fe5780649d014db43efab5d7d28737ac9
davidv410 <davidvrgoc99@gmail.com>
2026-07-19 16:20:34 +0200

    added data models

---
+++ Models/Drink.cs
@@ -0,0 +1,14 @@
+namespace SimpleCrud.Models;
+
+public class Drink
+{
+    public int Id  { get; set; }
+
+    public required string Name { get; set; }
+
+    public Flavour? Flavour { get; set; }
+
+    public int FlavourId { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+}
---
+++ Models/Flavour.cs
@@ -0,0 +1,8 @@
+namespace SimpleCrud.Models;
+
+public class Flavour
+{
+    public int Id  { get; set; }
+
+    public required string Name { get; set; }
+}
